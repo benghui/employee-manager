c8e7daa512c00e2a4a92890e4bd31a384f9fa65d
benghui <benghui@gmail.com>
2022-08-27 15:20:42 +0800

    Add routing to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 
 import { Routes, Route, Navigate } from 'react-router-dom';
-import List from './components/List'
+import List from './components/List';
+import Add from './components/Add';
+import Edit from './components/Edit';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="employee">
           <Route path="list" element={<List />} />
+          <Route path="add" element={<Add />} />
+          <Route path="edit" element={<Edit />} />
         </Route>
         <Route path="*" element={<Navigate to="/employee/list" replace />} />
       </Routes>
