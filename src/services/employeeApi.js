31476897cb18bfac5eafa51b17f667b8d356b636
benghui <benghui@gmail.com>
2022-08-28 23:01:45 +0800

    Update endpoints for createApi

--- src/services/employeeApi.js
+++ src/services/employeeApi.js
@@ -8,17 +8,42 @@ export const employeeApi = createApi({
 		getEmployees: builder.query({
 			query: () => '/',
 		}),
+		getEmployee: builder.query({
+			query: (employeeId) => `/${employeeId}`,
+			providesTags: (result, error, arg) => [{ type: 'Employee', id: arg }],
+		}),
 		addNewEmployee: builder.mutation({
 			query: initialEmployee => ({
 				url: '',
 				method: 'POST',
 				body: initialEmployee
-			})
-		})
+			}),
+			invalidatesTags: ['Employee'],
+		}),
+		editEmployee: builder.mutation({
+			query: (employee) => ({
+				url: `/${employee.id}`,
+				method: 'PATCH',
+				body: employee,
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: 'Employee', id: arg.id }],
+		}),
+		deleteEmployee: builder.mutation({
+			query: (id) => ({
+				// url: `/${id}`,
+				url: ``,
+				method: 'DELETE',
+				credentials: 'include',
+			}),
+			invalidatesTags: ['Employee'],
+		}),
 	}),
 })
 
 export const {
 	useGetEmployeesQuery,
-	useAddNewEmployeeMutation
+	useGetEmployeeQuery,
+	useAddNewEmployeeMutation,
+	useEditEmployeeMutation,
+	useDeleteEmployeeMutation
 } = employeeApi
